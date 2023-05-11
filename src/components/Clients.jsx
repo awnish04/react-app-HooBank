855c48026a0f7da251688d0eacb41d5588754263
awnish_09 <awnish.mehta@festnepal.com>
2023-05-11 12:24:59 +0545

    clients logo updated

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,8 +5,8 @@ const Clients = () => (
     <section id='clients' className={`${styles.flexCenter} `}>
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map ((client) => (
-          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-32 min-w-48`}>
-            <img src={client.logo} alt="client" className='sm:w-32 w-48 object-contain'/>
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
+            <img src={client.logo} alt="client" className='sm:w-48 w-24 object-contain'/>
           </div>
         ))}
 
